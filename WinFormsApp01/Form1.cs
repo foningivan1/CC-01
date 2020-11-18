0a6ed0dec021463f5284c2d23ce1ab93067d77a0
foning <ivanfoning1@gmail.com>
2020-11-18 14:11:48 +0100

    renommer CC01 en CC-01

--- WinFormsApp01/Form1.cs
+++ WinFormsApp01/Form1.cs
@@ -16,5 +16,10 @@ namespace WinFormsApp01
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }

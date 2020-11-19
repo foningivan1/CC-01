d31cb7de2b806dad329a98695295adf5d8f1d6c6
ramzy03 <59647043+ramzy03@users.noreply.github.com>
2020-11-19 16:35:10 +0100

    ajout des classes BO (Etudiant et Ecole)

---
+++ CC01.BO/Ecole.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC01.BO
+{
+    public class Ecole
+    {
+        private string Etablissement;
+        private string Site;
+        private string Tel;
+
+        public Ecole(string Etablissement, string Site, string Tel)
+        {
+            this.Etablissement = Etablissement;
+            this.Site = Site;
+            this.Tel = Tel;
+        }
+
+    }
+}
---
+++ CC01.BO/Etudiant.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC01.BO
+{
+    public class Etudiant
+    {
+        private string Lastname;
+        private string Firstname;
+        private string BornOn;
+        private string At;
+        private string Identifiant;
+        private int Contact;
+        private string Email;
+
+        public Etudiant(string Lastname, string Firstname, string BornOn, string At, string Identifiant, int Contact, string Email)
+        {
+            this.Lastname = Lastname;
+            this.Firstname = Firstname;
+            this.BornOn = BornOn;
+            this.At = At;
+            this.Identifiant = Identifiant;
+            this.Contact = Contact;
+            this.Email = Email;
+
+        }
+
+    }
+}
